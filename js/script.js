5e001c9f862e55f097abdcf061e10a065b8785fc
manikllcg <manik.badshallcg@gmail.com>
2023-12-01 17:41:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$('.myCarosul').owlCarousel({
+		items: 3,
+		margin: 30,
+		loop:true,
+		// center: true,
+		mouseDrag: true,
+		touchDrag: true,
+		startPosition: 2,
+		nav: true,
+
+		autoplayHoverPause: true,
+		smartSpeed: 110,
+		
+		
+		
+		
+				smartSpeed: 110,
+		responsive: {
+			767: {
+				items: 3
+			},
+			575: {
+				items: 2
+			},
+			320: {
+				items: 1
+			},
+			
+		},
+	});
+});
+$(document).ready(function(){
+    
+    $('.mobile-nav').click(function(){
+       $('.main-menu').toggleClass('Navclose');
+    });
+    $('.close-mobile-nav').click(function(){
+       $('.main-menu').addClass('Navclose');
+    });
+	
+})
